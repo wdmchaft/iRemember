d3834a100051506ed67d5b2d2af4990a162c0580
Aldrian Obaja <aldrian_math@yahoo.co.id>
2011-03-29 20:43:22 +0700

    Added the missing IRHeader.h

---
+++ Storage/IRHeader.h
@@ -0,0 +1,14 @@
+//
+//  IRConstants.h
+//  iRemember
+//
+//  Created by Aldrian Obaja Muis on 3/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#define DEFAULT_LANGUAGE @"Indonesian"
+
+typedef enum {
+	kGameStartAll = 0,
+	kGameStartStudied = 1,
+} GameStartMode;
